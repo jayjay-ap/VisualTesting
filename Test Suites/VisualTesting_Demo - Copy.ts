bba2947edaadfc01c268bc00b950f5c461e8cd8f
jpaguirigan <jpaguirigan@PH-5CG0021YLW>
2024-02-12 19:47:24 +0800

    testing

---
+++ Test Suites/VisualTesting_Demo - Copy.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>VisualTesting_Demo - Copy</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>deae85a3-793a-46b9-9103-80bd0d96e546</testSuiteGuid>
+   <testCaseLink>
+      <guid>a24e336a-0521-4da1-8c36-9fa74cc282d0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CreateAppointment</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
